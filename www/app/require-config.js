a4d869a295c670bbb624665fe0035fd67e60970c
Joe Stump <joe@joestump.net>
2013-12-29 16:26:49 -0800

    Fix the path to Moment.js.

--- www/app/require-config.js
+++ www/app/require-config.js
@@ -7,7 +7,7 @@ require.config({
     backbone: '../bower_components/backbone/backbone',
     underscore: '../bower_components/lodash/lodash',
     jquery: '../bower_components/jquery/jquery',
-    moment: '../bower_components/moment/moment',
+    moment: '../bower_components/momentjs/moment',
     tpl: '../bower_components/lodash-template-loader/loader',
     i18n: '../bower_components/requirejs-i18n/i18n',
     text: '../bower_components/requirejs-text/text',
